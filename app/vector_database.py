c97c7ed2fed1390f8b39c48af6d6bd09d16ad471
Jan <janek.r559@icloud.com>
2025-10-29 17:46:51 +0100

    fix: reading qdrant url from env

--- app/vector_database.py
+++ app/vector_database.py
@@ -1,11 +1,14 @@
 # Vector database
+import os
 from qdrant_client.models import Distance, VectorParams
 from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
 
 import process_data
 
-vector_database_client = QdrantClient(url="http://localhost:6333")
+# Support both Docker and local setups
+QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
+vector_database_client = QdrantClient(url=QDRANT_URL)
 
 vector_size = len(process_data.embeddings[0]['embedding'])
 
